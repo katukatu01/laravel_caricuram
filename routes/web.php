5dc62b4c757916be9bc2b2ba8c30e38eddad41ea
katukatu01 <1910370183d@kindai.ac.jp>
2021-08-07 02:09:04 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,6 +12,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', 'PostController@index');
