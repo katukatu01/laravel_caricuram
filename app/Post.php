111e13d49abe47062d1a1d0948c78c57eb3495d6
katukatu01 <1910370183d@kindai.ac.jp>
2021-08-24 10:17:19 +0900

    dev_basis04 add

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+    }
+    
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
+?>
