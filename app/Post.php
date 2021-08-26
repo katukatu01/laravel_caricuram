6fcfc297f0620bebb8988c17f00ee8a6236d3c47
katukatu01 <1910370183d@kindai.ac.jp>
2021-08-26 10:41:46 +0900

    完成

---
+++ app/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::get('/posts/{post}', 'PostController@show');
+Route::delete('/posts/{post}', 'PostController@delete');
+Route::post('/posts', 'PostController@store');
+Route::put('/posts/{post}', 'PostController@update');
+?>
